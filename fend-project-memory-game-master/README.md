c668dc615e3959a579142677cb349a9f007dd309
abujawsa <49504866+abujawsa@users.noreply.github.com>
2019-04-16 12:03:55 -0400

    Add files via upload
    improved CSS, updated Readme w/ actions & sources, updated HTML and JavaScript files. Thank you for your review!

---
+++ fend-project-memory-game-master/README.md
@@ -0,0 +1,48 @@
+# Memory Game Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+
+
+## HTML Document
+- Changed Heading to Flash's Matching Game
+- Added DIV classes for modal
+- Flipped cards face down
+
+
+## JavaScript File
+- Functions for time, modal, game start, end, reset, retry, award system, moves
+- Shuffle
+- Event listeners
+- Manipulated DOM
+
+## CSS File
+- New background, source:  https://thewallpaper.co/preview/?wallpaper=new-england-patriots-backgrounds-for-desktop-desktop-wallpapers-hd-high-definition-windows-10-mac-apple-colourful-images-backgrounds-free-1920x1080*/
+- Reformatting of score panel
+- Changed card colors and sizes
+- Styled modal
+
+## Helpful Resources/Citations used:
+- Background:  https://thewallpaper.co/preview/?wallpaper=new-england-patriots-backgrounds-for-desktop-desktop-wallpapers-hd-high-definition-windows-10-mac-apple-colourful-images-backgrounds-free-1920x1080*/
+- Cursors: https://www.w3schools.com/csSref/pr_class_cursor.asp
+- Colors: https://www.w3schools.com/cssref/css_colors.asp
+- Fonts: https://www.w3schools.com/Css/css_font.asp
+- Timer Function: https://matthewcranford.com/memory-game-walkthrough-part-6-the-clock/
+- Transform property: https://www.w3schools.com/cssref/css3_pr_transform.asp
---
+++ fend-project-memory-game-master/js/app.js
@@ -0,0 +1,246 @@
+
+//*GLOBAL VARIABLES*//
+
+let deck = document.querySelector(".deck");
+const cardsToShuffle = Array.from(document.querySelectorAll(".card"));
+const starsAwarded = document.querySelectorAll(".stars li");
+let openedCards = [];
+let moves = 0;
+let timerOff = true;
+let time = 0;
+let matched = 0;
+let timerId;
+
+//*Pass shuffle function in newly created array from .card element. Result stored in shuffledCards.*//
+function shuffleDeck() {
+   const shuffledCards = shuffle(cardsToShuffle);
+/*Append shuffled card to deck, add to DOM - changing order of original object*/
+    for (card of shuffledCards) {
+        deck.appendChild(card);
+    }
+}
+shuffleDeck();
+
+//* Shuffle function from http://stackoverflow.com/a/2450976*//
+function shuffle(array) {
+    var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/* set up the event listener for a card. If a card is clicked:
+ * display the card's symbol (put this functionality in another function that you call from this one)
+ * add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * if the list already has another card, check to see if the two cards match
+ * if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ * if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)*
+ * call function to increment moves counter when both cards are flipped
+ */
+
+ deck.addEventListener("click", event => {
+   const card = event.target;
+   if (
+     card.classList.contains("card") &&
+      !card.classList.contains("match") &&
+      openedCards.length < 2 &&
+      !openedCards.includes(card)
+    ) {
+        if (timerOff) {
+          startTimer();
+          timerOff = false;
+        }
+       flipCard(card);
+       addOpenCard(card);
+   if (openedCards.length === 2) {
+   checkOpenCards();
+   incrementMove();
+   checkScore();
+   }
+ }
+ });
+
+ //*Function to start timer*//
+ function startTimer() {
+    let timerId = setInterval(() => {
+         time++;
+         showTime();
+     }, 1000);
+ }
+ startTimer()
+
+//Show time using built in Match function to round down decimals and show seconds in correct format.*//
+ function showTime() {
+   const timer = document.querySelector(".timer");
+   const minutes = Math.floor(time / 60);
+   const seconds = time % 60;
+
+   if (seconds < 10) {
+        timer.innerHTML = `${minutes}:0${seconds}`;
+    } else {
+        timer.innerHTML = `${minutes}:${seconds}`;
+    }
+  }
+
+/* Function to display the open cards on the page*/
+function flipCard(card) {
+  card.classList.toggle("open");
+  card.classList.toggle("show");
+}
+
+/* Function to add card to a list of opened cards */
+function addOpenCard(card) {
+  openedCards.push(card);
+}
+
+//*Function to check if 2 opened cards match and reset after a certain amount of time if they don't. once they all match,execute endGame*//
+function checkOpenCards() {
+  const total_Matches = 8;
+
+    if (
+        openedCards[0].firstElementChild.className ===
+        openedCards[1].firstElementChild.className
+    ) {
+        openedCards[0].classList.toggle("match");
+        openedCards[1].classList.toggle("match");
+        openedCards = [];
+        matched++;
+        if (matched === total_Matches) {
+          endGame();
+        }
+    } else {
+      setTimeout(() => {
+        flipCard(openedCards[0]);
+        flipCard(openedCards[1]);
+        openedCards = [];
+      }, 1000);
+    }
+  }
+
+  //*Function to increment the move counter and modify moves HTML to show new total moves based on increment*//
+  function incrementMove() {
+    moves++;
+    const movesCounter = document.querySelector(".moves");
+    movesCounter.innerHTML = `Moves: ${moves}`
+  }
+
+  //*Function to check score based on how many moves were made and remove star at specified intervals*//
+  function checkScore() {
+    if (moves === 7 || moves === 12 || moves === 17){
+      loseStar();
+    }
+  }
+
+  //*Function to disappear star that will be run within checkScore function*//
+  function loseStar() {
+    for (star of starsAwarded) {
+      if (star.style.display !== "none") {
+        star.style.display = "none";
+        break;
+    }
+  }
+  }
+
+//*Function to stop time*//
+function stopTimer() {
+  clearInterval(timerId);
+  }
+
+function endGame() {
+  stopTimer();
+  toggleModal();
+  showModalScoreboard();
+}
+
+//*Function to show and hide modal popup that will be called in other functions*//
+function toggleModal() {
+  const modal = document.querySelector(".modal-background");
+  modal.classList.toggle("hide");
+}
+
+//Ccounts # of stars achieved and return #*//
+function getStars() {
+    starCount = 0;
+    for (star of starsAwarded) {
+        if (star.style.display !== "none") {
+            starCount++;
+        }
+    }
+    return starCount;
+}
+
+//*Data to be displayed on Modal, modify HTML to present gathered time, score, moves, stars.*//
+
+function showModalScoreboard() {
+    const timeStat = document.querySelector(".modal-time");
+    const recordedTime = document.querySelector(".timer").innerHTML;
+    const movesNeeded = document.querySelector(".modal-moves");
+    const starsAchieved = document.querySelector(".modal-stars");
+    const stars = getStars();
+
+    starsAchieved.innerHTML = `Stars Achieved = ${stars}`;
+    timeStat.innerHTML = `Recorded Time = ${recordedTime}`;
+    movesNeeded.innerHTML = `Moves Needed = ${moves}`;
+}
+
+//*Listens and calls for replay and return*//
+document.querySelector(".modal_return").addEventListener("click", () => {
+  toggleModal();
+});
+document.querySelector(".modal_replay").addEventListener("click", replayGame);
+
+//*Function to hide modal and reset Game*//
+function replayGame() {
+    resetGame();
+    toggleModal();
+}
+
+//*Function to reset cards to default, not show*//
+function resetCards() {
+  const cards = document.querySelectorAll(".deck li");
+  for (let card of cards) {card.className = "card";}
+}
+
+//*Function so moves reset and display as 0*//
+function resetMoves() {
+  moves = 0;
+  document.querySelector(".moves").innerHTML = "Moves: " + moves;
+
+}
+
+//*Function so stars reset and display as 3 stars*//
+function resetStars() {
+  for (star of starsAwarded) {
+    star.style.display = "inline";
+  }
+}
+
+//*Calls reset function upon selection of restart class*//
+document.querySelector('.restart').addEventListener('click', resetGame);
+
+//*Resets all aspects of game (Moves to 0, time to 00:00, stars to 3) and shuffles deck*//
+function resetGame() {
+  resetTimerandDisplay();
+  resetMoves();
+  resetStars();
+  resetCards();
+  shuffleDeck();
+}
+
+
+//*Stops time, resets and displays as 0*//
+function resetTimerandDisplay() {
+  stopTimer();
+  timerOff = true;
+  time = 0;
+  showTime();
+}
